95b67b3c2f3cf3de171655a6349cc7a41467db38
kyuing <kyykyu000@gmail.com>
2021-03-24 23:29:03 +0000

    v0.0.5: wk06 item-controller.js and mongodb init setting

---
+++ item-controller.js
@@ -0,0 +1,17 @@
+// get /hello
+exports.getWorld = function(req,res){
+    res.json({result: 'Hello Wrold from Controller'});
+}
+
+// get /hello/:foo/:bar
+exports.getWorldParams = function(req,res){
+    res.json({message: 'Hello BScBest!', data: [
+        req.params.foo,
+        req.params.bar
+    ]});
+};
+
+// post /hello
+exports.postWorld = function(req,res){
+    res.json({result: 'Post was sent', data: req.body});
+};
--- routes.js
+++ routes.js
@@ -1,16 +1,17 @@
 const express = require('express'),
 router = express.Router();
 
-router.get('/hello/:foo/:bar', (req, res) => {
-    res.json({message: 'Hello BScBest!', data: [
-        req.params.foo,
-        req.params.bar
-    ]});
-});
+var itemCtrl = require('./item-controller');  //ref to item-controller.js
+//userCtrl = require('./user-controller');
 
-router.post('/hello', (req, res) => {
-    res.json({result: 'post sent!', data: req.body});
-});
+router.get('/hello', itemCtrl.getWorld);
+
+router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
+
+router.post('/hello', itemCtrl.postWorld);
+
+// router.post('/users', userCtrl.createUser);
+// router.get('/users', userCtrl.getUsers);
 
 /* In index.js, which can better be named server.js, 
  * the code 
